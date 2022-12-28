8031027ac2f58460ae6ecb0a661c62e22c156f99
LDDW <drouinbenjamin20@gmail.com>
2022-12-28 20:45:14 +0100

    make interface and function to create data table

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
-// 👇️ named import
+// import checker
 import {Checker} from './checker.js';
-
+//init new checker
 const checker = new Checker();
-checker.check();
+// create table with data
+const table = document.getElementById('table');
+table.innerHTML = await checker.setHtmlTable(table);
+// check content of textarea
+const textarea = document.getElementById('textarea');
+const loader = document.getElementById('loader');
+let timeoutId;
+
+textarea.addEventListener('keyup', function(event) {
+    loader.classList.remove('hidden');
+
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+        loader.classList.add('hidden');
+        checker.check(textarea.textContent);
+    }, 2000);
+});
+
+//counter
--- js/checker.js
+++ js/checker.js
@@ -1,17 +1,87 @@
 export class Checker {
 
-    async #getJson(){
-        return fetch('./js/checker.json')
+    /**
+     * @function getJson
+     * @returns object
+     */
+    #getJson(){
+        try {
+            return fetch('./js/checker.json')
             .then((response) => response.json())
-            .then((data) => {
-                return data;
-            })
-            .catch((error) => console.log(error));
+            .then((data) => data)
+        } catch (error) {
+            console.log(error);  
+        } 
+    }   
+
+    /**
+     * @function getArrayWithAllWords
+     * @returns array with all words store in json file 'checker.json'
+     */
+    async #getArrayWithAllWords(){
+        const getJson = await this.#getJson();
+        let array = [];
+        getJson.categories.forEach(category => {
+            category.words.forEach(word => {
+                array.push(word.toLowerCase());
+            });
+        });
+        return array;
     }
-  
-    async check(){
-        const a = await this.#getJson();
-        console.log(a);
+    
+    /**
+     * @async 
+     * @function check the user email
+     * @param {*} object 
+     */
+    async check(data){
+        try {
+            if(data !== undefined && data !== ''){
+                const arrayWithAllWords = await this.#getArrayWithAllWords();
+                arrayWithAllWords.forEach(word => {
+                    if(data.toLowerCase().indexOf(word) > - 1){
+                        console.log(word)
+                    }
+                })
+            } else {
+                throw 'data is empty';
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }   
+
+    /**
+     * @async 
+     * @function setHtmlTable
+     * @param {*} object is table element
+     * @returns html of table
+     */
+    async setHtmlTable(element) {
+        const getJson = await this.#getJson();
+        let html = '';
+        try {
+            if(typeof(element) !== 'object'){
+                throw 'fz ';
+            }
+            getJson.categories.forEach(category => {
+                html += `
+                <tr>
+                    <td class="p-3 border-[1px] border-gray-300 bg-gray-200">${category.name}</td>
+                    <td class="p-3 border-[1px] border-gray-300 bg-gray-50">
+                `;
+                category.words.forEach(word => {
+                    html += `${word}, `;
+                });
+                html += `
+                    </td>
+                </tr>
+                `;
+            });
+        } catch (error) {
+            console.log(error)
+        }
+        return html;
     }
 
 }
